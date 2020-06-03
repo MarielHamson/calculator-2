9820f9a6b38e86a562474cfe19e525966e4e5984
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-03 14:47:40 -0700

    add form and logic for cooking conversion
    Co-Authored-by: Nikolas Kososik <nikolas.kososik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ const add = function(number1, number2) {
   };
   
 const subtract = function(number1, number2) {
-    return number1 - number2;
+  return number1 - number2;
     };
   
 const multiply = function(number1, number2) {
@@ -16,6 +16,10 @@ const divide = function(number1, number2) {
   return number1 / number2;
     };
 
+const convertc2tb = function(number1) {
+  return number1 * 16
+    };
+
   //User Interface logic//
   
 $(document).ready(function() {
@@ -24,7 +28,7 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    $("#output1").text(result);
   });
 
   $("form#subtract").submit(function(event) {
@@ -32,7 +36,7 @@ $(document).ready(function() {
     const number1 = parseInt($("#subtract1").val());
     const number2 = parseInt($("#subtract2").val());
     const result = subtract(number1, number2);
-    $("#output").text(result);
+    $("#output2").text(result);
   });
 
   $("form#multiply").submit(function(event) {
@@ -40,7 +44,7 @@ $(document).ready(function() {
     const number1 = parseInt($("#multiply1").val());
     const number2 = parseInt($("#multiply2").val());
     const result = multiply(number1, number2);
-    $("#output").text(result);
+    $("#output3").text(result);
   });
 
   $("form#divide").submit(function(event) {
@@ -48,7 +52,14 @@ $(document).ready(function() {
     const number1 = parseInt($("#divide1").val());
     const number2 = parseInt($("#divide2").val());
     const result = divide(number1, number2);
-    $("#output").text(result);
+    $("#output4").text(result);
+  });
+
+  $("form#convert").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#convert1").val());
+    const result = convertc2tb(number1);
+    $("#output5").text(result);
   });
 
 });
