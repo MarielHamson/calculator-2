55a958ae72fc16613d52a5cdfe2fce55c72e5b3f
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-03 14:15:34 -0700

    add form fields for subtraction
    Co-Authored-by: Nikolas Kososik <nikolas.kososik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ const add = function(number1, number2) {
   return number1 + number2;
   };
   
-  const subtract = function(number1, number2) {
+const subtract = function(number1, number2) {
     return number1 - number2;
     };
   
@@ -24,6 +24,14 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
     $("#output").text(result);
   
     });
